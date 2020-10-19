4a688fcb221bbc6d0499536ecaef30cddb52b967
Vapak <46297246+Vapak@users.noreply.github.com>
2020-10-19 21:05:48 +0700

    components + async GET + filter + addNews

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // подключение библиотеки React
+import {News} from './components/News';
+import {Add} from './components/Add';
+import './App.css'; // подключение файла стилей
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// далее скопировано из тэга script
+
+class App extends React.Component {
+  state = {
+    news: null,
+    isLoading:false
+  }
+
+  static getDerivedStateFromProps(props, state){
+    if(Array.isArray(state.news)){
+        let nextFilteredNews = [...state.news]
+
+        nextFilteredNews.forEach((item, index) => {
+        if (item.bigText.toLowerCase().includes('pubg')) {
+          item.bigText = 'СПАМ'
+        }
+      })
+      return {
+        filteredNews: nextFilteredNews,
+      }
+    }
+  return null
 }
 
+  componentDidMount() {
+    this.setState({isLoading:true})
+     fetch('http://localhost:3000/data/newsData.json')
+       .then(response => {
+         return response.json()
+       })
+       .then(data => {
+         setTimeout(()=>{
+            this.setState({news:data, isLoading:false})
+         }, 3000)
+       })
+   }
+  handleAddNews = data => {
+    const nextNews = [data, ...this.state.news];
+    this.setState({ news: nextNews });
+  };
+  render() {
+    const {news, isLoading} = this.state;
+    return (
+      <React.Fragment>
+        <Add onAddNews={this.handleAddNews} />
+        <h3>Новости</h3>
+        {isLoading && <p>Загружаю...</p>}
+        {Array.isArray(news) && <News data={news} />}
+      </React.Fragment>
+    );
+  }
+}
+
+// скопировано все кроме ReactDOM.render
+
+// добавился export
 export default App;
